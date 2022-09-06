8e9b753d603aed98eae0cec29346b326e5e8e977
Zachariah Mully <zack.mully@zephyrai.bio>
2022-09-06 13:53:58 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,16 @@
+import { cdk, javascript } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Zachariah Mully',
+  authorAddress: 'zack.mully@zephyrai.bio',
+  defaultReleaseBranch: 'main',
+  name: 'python-test-2',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/zack.mully/python-test-2.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
